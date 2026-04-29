5166e2a30f5f508d9a90f0593cf15b064a783846
Sergio Carracedo <hi@sergiocarracedo.es>
2026-04-29 21:26:49 +0200

    fix: configure web for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://sergiocarracedo.github.io",
-  base: "/skill-organizer",
+  site: "https://skill-organizer.sergiocarracedo.es",
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
